8f9b04482deba3492b4fae63707962f05e8672c1
assem22 <assem.mukhamadi@mail.ru>
2021-03-15 01:27:08 +0600

    all

---
+++ src/main/java/kz/iitu/midtermSpringProject/MidtermSpringProjectApplication.java
@@ -0,0 +1,13 @@
+package kz.iitu.midtermSpringProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MidtermSpringProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MidtermSpringProjectApplication.class, args);
+	}
+
+}
